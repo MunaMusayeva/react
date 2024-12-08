009607c7c5caccab9873bc09dbe9e178987a0252
USER <USER@DESKTOP-JI95IOS>
2024-12-08 22:37:37 +0400

    new

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Register from './register/Register'
+import Login from './login/Login'
+
+const App = () => {
+  return (
+    <>
+
+      <Login />
+    </>
+  )
+}
+
+export default App
